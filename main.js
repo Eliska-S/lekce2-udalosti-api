c57a0d1172309fa0add0193c3a062197125c3070
Eliska_S <el.sibrova@gmail.com>
2021-03-19 18:06:03 +0100

    [HOMEWORK] Lekce 2

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
-// tady je místo pro náš program
+// Samostatná práce v hodině a domácí úkol 1 a 2.
+
+function ztucniPismo () {
+    document.querySelector('.odstavec').classList.toggle('tucny');
+}
+
+function zmenaBarvy () {
+    document.querySelector('.odstavec').classList.toggle('cervena');
+}
+
+let currentSize = 16;
+
+function zvetsiPismo () {
+    currentSize++;
+    document.querySelector('.odstavec').style.fontSize = currentSize + "px";
+}
+
+// Domácí úkol 3 (API).
+
+let zvuk = document.getElementById('js-audio');
+
+function prehraj () {
+    zvuk.play();
+}
+
+function pozastav () {
+    zvuk.pause();
+}
+
+function ztlum () {
+    zvuk.volume = 0;
+}
+
+function nastavHlasitost () {
+    zvuk.volume = 0.5;
+}
+
+function zvysHlasitost () {
+    zvuk.volume = 1;
+}
+
+function vratNaZacatek () {
+    zvuk.currentTime = 0;
+}
+
